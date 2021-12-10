f96845a585df355ea4241e00aa1173e760e7a4be
sieun <sieun0115@ajou.ac.kr>
2021-12-10 15:53:43 +0900

    More simple code of Button.js

--- react/begin-react/src/components/Button.js
+++ react/begin-react/src/components/Button.js
@@ -18,26 +18,28 @@ const colorStyles = css`
     }}
 `
 
+const sizes = {
+    large: {
+        height: '3rem',
+        fontSize: '1.25rem'
+    },
+    medium: {
+        height: '2.25rem',
+        fontSize: '1rem'
+    },
+    small: {
+        height: '1.75rem',
+        fontSize: '0.875rem'
+    }
+}
+
 const sizeStyles = css`
     /* 크기 */
-    ${props => 
-        props.size === 'large' && css`
-            height: 3rem;
-            font-size: 1.25rem;
-        `
-    }
-    ${props => 
-        props.size === 'medium' && css`
-            height: 2.25rem;
-            font-size: 1rem;
-        `
-    }
-    ${props => 
-        props.size === 'small' && css`
-            height: 1.75rem;
-            font-size: 0.875rem;
-        `
-    }
+    ${({ size }) => css`
+        height: ${sizes[size].height};
+        fonst-size: ${sizes[size].fontSize};
+    `}
+
 `
 
 const StyledButton = styled.button`
