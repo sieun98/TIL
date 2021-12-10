56143489d061510cb4c8768dab7e0b2ebebfd02f
sieun <sieun0115@ajou.ac.kr>
2021-12-10 15:52:08 +0900

    Button Component with styeld-components

--- react/begin-react/src/App.js
+++ react/begin-react/src/App.js
@@ -1,86 +1,60 @@
-import React, {
-  useRef,
-  useReducer,
-  useCallback,
-  useMemo,
-  createContext,
-} from "react";
-import UserList from "./UserList";
-import CreateUser from "./CreateUser";
-import useInputs from "./useInputs";
-import produce from "immer";
+import React from "react";
+import styled, { css, ThemeProvider } from "styled-components";
+import Button from "./components/Button";
 
-function countActiveUsers(users) {
-  console.log("활성 사용자 수를 세는 중...");
-  return users.filter((user) => user.active).length;
-}
+const Circle = styled.div`
+  width: 5rem;
+  height: 5rem;
+  background: ${(props) => props.color};
+  border-radius: 50%;
+  ${(props) =>
+    props.huge &&
+    css`
+      width: 10rem;
+      height: 10rem;
+    `}
+`;
 
-const initialState = {
-  users: [
-    {
-      id: 1,
-      username: "sieun",
-      email: "sieun@naver.com",
-      active: true,
-    },
-    {
-      id: 2,
-      username: "test1",
-      email: "test1@naver.com",
-      active: false,
-    },
-    {
-      id: 3,
-      username: "test2",
-      email: "test2@naver.com",
-      active: false,
-    },
-  ],
+const AppBlock = styled.div`
+  width: 512px;
+  margin: 0 auto;
+  margin-top: 4rem;
+  border: 1px solid black;
+  padding: 1rem;
+`;
+
+const ButtonGroup = styled.div`
+  & + & {
+    margin-top: 1rem;
+  }
+`;
+
+const palette = {
+  blue: "#228be6",
+  gray: "#496057",
+  pink: "#f06595",
 };
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "CREATE_USER":
-      return {
-        users: state.users.concat(action.user),
-      };
-    case "TOGGLE_USER":
-      return produce(state, (draft) => {
-        const user = draft.users.find((user) => user.id === action.id);
-        user.active = !user.active;
-      });
-    case "REMOVE_USER":
-      return {
-        ...state,
-        users: state.users.filter((user) => user.id !== action.id),
-      };
-    default:
-      throw new Error("Unhandled action");
-  }
-}
-
-export const userDispatch = createContext(null);
-
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  const { users } = state;
-  // const [form, onChange, reset] = useInputs({
-  //   username: '',
-  //   email: ''
-  // })
-
-  // const {username, email} = form
-
-  // const nextId = useRef(4)
-
-  const count = useMemo(() => countActiveUsers(users), [users]);
-
   return (
-    <userDispatch.Provider value={dispatch}>
-      <CreateUser />
-      <UserList users={users} />
-      <div>활성 사용자 수: {count}</div>
-    </userDispatch.Provider>
+    <ThemeProvider theme={{ palette }}>
+      <AppBlock>
+        <Circle color="red" huge />
+        <Circle color="green" />
+        <ButtonGroup>
+          <Button>BUTTON</Button>
+          <Button color="pink">BUTTON</Button>
+          <Button color="gray">BUTTON</Button>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="large">BUTTON</Button>
+          <Button color="pink">BUTTON</Button>
+          <Button color="gray" size="small">
+            BUTTON
+          </Button>
+        </ButtonGroup>
+      </AppBlock>
+    </ThemeProvider>
   );
 }
 
---
+++ react/begin-react/src/components/Button.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import styled, { css } from 'styled-components'
+import { darken, lighten } from 'polished'
+
+const colorStyles = css`
+    /* 색상 */
+    ${({ theme, color }) => {
+        const selected = theme.palette[color];
+        return css`
+            background: ${selected};
+            &:hover {
+                background: ${lighten(0.1, selected)};
+            }
+            &:active {
+                background: ${darken(0.1, selected)};
+            }
+        `
+    }}
+`
+
+const sizeStyles = css`
+    /* 크기 */
+    ${props => 
+        props.size === 'large' && css`
+            height: 3rem;
+            font-size: 1.25rem;
+        `
+    }
+    ${props => 
+        props.size === 'medium' && css`
+            height: 2.25rem;
+            font-size: 1rem;
+        `
+    }
+    ${props => 
+        props.size === 'small' && css`
+            height: 1.75rem;
+            font-size: 0.875rem;
+        `
+    }
+`
+
+const StyledButton = styled.button`
+    /* 공통 스타일 */
+    display: inline-flex;
+    align-items: center;
+    outline: none;
+    border: none;
+    border-radius: 4px;
+    color: white;
+    font-weight: bold;
+    cursor: pointer;
+    padding-left: 1rem;
+    padding-right: 1rem;
+
+    /* 크기 */
+    ${sizeStyles}
+    /* 색상 */
+    ${colorStyles}
+
+    & + & {
+        margin-left: 1rem;
+    }
+`
+
+function Button({ children, color, size,  ...rest}) {
+    return (
+        <StyledButton color={color} size={size} {...rest}>{children}</StyledButton>
+    )
+}
+
+Button.defaultProps = {
+    color: 'blue',
+    size: 'medium'
+}
+
+export default Button
