99cb6dcdf8fdb698206f5f24e755fa1c46545184
sieun <sieun0115@ajou.ac.kr>
2022-04-08 19:04:36 +0900

    connect 대신 useSelect, useDispatch 사용

--- react/redux-example/src/containers/TodoFormContainer.jsx
+++ react/redux-example/src/containers/TodoFormContainer.jsx
@@ -1,14 +1,19 @@
-import { connect } from "react-redux";
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
 import TodoForm from "../components/TodoForm";
 import { addTodo } from "../redux/actions";
 
-const TodoFormContainer = connect(
-  (state) => ({}),
-  (dispatch) => ({
-    add: (text) => {
+function TodoFormContainer() {
+  const dispatch = useDispatch();
+
+  const add = useCallback(
+    (text) => {
       dispatch(addTodo(text));
     },
-  })
-)(TodoForm);
+    [dispatch]
+  );
+
+  return <TodoForm add={add} />;
+}
 
 export default TodoFormContainer;
