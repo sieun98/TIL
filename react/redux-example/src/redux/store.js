fab3311cae8b43915e8131462bfdabe4430a020f
sieun <sieun0115@ajou.ac.kr>
2022-04-08 23:55:41 +0900

    redux-devtools

--- react/redux-example/src/redux/store.js
+++ react/redux-example/src/redux/store.js
@@ -1,20 +1,7 @@
 import { applyMiddleware, createStore } from "redux";
 import reducer from "./reducers/reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 
-function middleware1(store) {
-  console.log("middleware1", 0);
-  return (next) => {
-    console.log("middleware1", 1);
-    return (action) => {
-      console.log("middleware1", 2);
-      const returnValue = next(action);
-      console.log("middleware1", 3);
-
-      return returnValue;
-    };
-  };
-}
-
-const store = createStore(reducer, applyMiddleware(middleware1));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware()));
 
 export default store;
